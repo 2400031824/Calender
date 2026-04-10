ed3a0e87833593f926a6ecbd9738adc6741d5c5e
adithi-k-max <adithikotapothula@gmail.com>
2026-04-10 13:44:37 +0530

    feat: add download CTA on website

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,26 @@
-﻿import WallCalendar from '../components/WallCalendar';
+import WallCalendar from '../components/WallCalendar';
 
 export default function Home() {
-  return <WallCalendar />;
+  const downloadUrl = 'https://github.com/2400031824/Calender/releases/latest';
+  const githubUrl = 'https://github.com/2400031824/Calender';
+
+  return (
+    <>
+      <WallCalendar />
+      <div className="download-cta">
+        <div className="download-cta__text">
+          <p className="download-cta__title">Wall Calendar Widget</p>
+          <p className="download-cta__subtitle">Download the Windows desktop app</p>
+        </div>
+        <div className="download-cta__actions">
+          <a className="download-cta__btn" href={downloadUrl} target="_blank" rel="noreferrer">
+            Download for Windows
+          </a>
+          <a className="download-cta__link" href={githubUrl} target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
